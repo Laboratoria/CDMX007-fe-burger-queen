8b9691ffc71e99893f9d8de6262c483f3fdd88b2
lawrayette <milhadasdepapel@gmail.com>
2019-05-02 23:23:24 -0500

    primer maquetado

---
+++ src/menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './menu.css';
+
+function Menu() {
+  return (
+    <div className="Menu">
+     <h5 className ="col m8 offset-m4"> Menú</h5>
+    </div>
+  );
+}
+
+export default Menu;
