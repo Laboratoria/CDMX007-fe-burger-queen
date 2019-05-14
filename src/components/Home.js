3d6cbf52f7605e1435df5033f1a247118e88d1ee
Abygail27 <abygaailrrocha@gmail.com>
2019-05-13 19:59:10 -0500

    installed semantic-ui-react

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,13 @@
-import React from 'react';
-import mainLogo from './logo.png';
+import React from 'react'
+import { Button, Icon } from 'semantic-ui-react'
 
-const Home = () => {
-    return (
-    <div className = "wrapper">
+const ButtonExampleLabeledIcon = () => (
+  <div>
+    <Button icon labelPosition='right'>
+      Next
+      <Icon name='right arrow' />
+    </Button>
+  </div>
+)
 
-    <div className= "header">
-    <img src= {mainLogo} alt = "burgerQueen"></img>
-    </div>
-
-    <div className = "content">
-    <input type = "password" id = "input-pa" placeholder= "introduce tu clave" name= "password"></input>
-    <button id="enter"><i class= "fas fa-check" to="/Menu"></i>entrar</button>
-    </div>
-    <div className = "footer"></div>
-   
-    </div>
-    
-    );
-};
-
-export default Home; 
+export default ButtonExampleLabeledIcon
