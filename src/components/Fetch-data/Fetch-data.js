2c70520f8d0a62417c22e776a4a653cc01866d5b
AndreaMyshkn <atrkvsk@hotmail.com>
2019-05-15 14:36:35 -0500

    Se implementó filtrado por tipo tanto para comida como para desayunos. Se añade estilos a la card donde se imprime el menú

--- src/components/Fetch-data/Fetch-data.js
+++ src/components/Fetch-data/Fetch-data.js
@@ -41,13 +41,37 @@ class FetchData extends Component{
                          <div className="col l10 offset-l1 m10  offset-m1">
                          <div className="card-panel grey lighten-5">
                          <span className="card-style black-text">
-                           {items.filter(item=>item.type ==='Desayuno').map(item=><p>{item.product}</p>)}
 
+                         <div className="row">
+                     
+                         <ul>
+                         <h4 className=" center">Desayunos</h4>
 
+                             <li className= "col l6">{items.filter(item=>item.type ==='Desayuno').map(item=><p>{item.product}</p>)}</li></ul>
+                         <ul>
+                            
+                             
+                             <li className ="col l6"><h5>Bebidas</h5>{items.filter(item=>item.type ==='Bebida-Desayuno').map(item=><p>{item.product}</p>)}</li></ul>
+                      </div>   
+                     </span>  </div> </div>  </div>
+                     
+                 
+                             
+                    <div className="row ">
+                         <div className="col l10 offset-l1 m10  offset-m1">
+                         <div className="card-panel grey lighten-5">
+                         <span className="card-style black-text">
 
+                         
 
-                   
-                     </span>  </div> </div>  </div></div>
+                           {items.filter(item=>item.type ==='Comida').map(item=><p>{item.product}</p>)}
+                         
+                     </span>  </div> </div>  </div>
+                     
+                     
+                     
+                     
+                     </div>
                       
                
  
